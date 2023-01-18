14334a643da15e76db29d419242a82b1c39a8ffe
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-01-18 12:11:54 +0300

    Adding Price Filter Option to Products

--- resources/views/livewire/frontend/product/index.blade.php
+++ resources/views/livewire/frontend/product/index.blade.php
@@ -2,19 +2,37 @@
     <div class="row">
         <div class="col-md-3">
             @if($category->brands)
-            <div class="card">
-                <div class="card-header">
-                    <h4>Brands</h4>
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Brands</h4>
+                    </div>
+                    <div class="card-body">
+                        @foreach($category->brands as $brandItem)
+                            <label class="d-block">
+                                <input type="checkbox" wire:model="brandInputs"
+                                       value="{{$brandItem->name}}"/> {{$brandItem->name}}
+                            </label>
+                        @endforeach
+                    </div>
                 </div>
-                <div class="card-body">
-                    @foreach($category->brands as $brandItem)
+            @endif
+            <div class="card mt-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Price</h4>
+                    </div>
+                    <div class="card-body">
+                            <label class="d-block">
+                                <input type="radio" name="priceSort" wire:model="priceInput"
+                                       value="high-to-low"/> High to Low
+                            </label>
                         <label class="d-block">
-                            <input type="checkbox" wire:model="brandInputs" value="{{$brandItem->name}}" /> {{$brandItem->name}}
+                            <input type="radio" name="priceSort" wire:model="priceInput"
+                                   value="low-to-high"/> Low to High
                         </label>
-                    @endforeach
+                    </div>
                 </div>
             </div>
-            @endif
         </div>
         <div class="col-md-9">
             <div class="row">
