bc7cb1bdfe7808eee3c408e884ca7c008fb6b39a
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-01-30 09:43:32 +0300

    Adding Alertifyjs to Frontend Layout

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -17,7 +17,11 @@ class View extends Component
             if (Wishlist::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists())
             {
                 session()->flash('message', 'Already added to wishlist');
-
+                $this->dispatchBrowserEvent('message', [
+                    'text' => 'Already added to wishlist',
+                    'type' => 'warning',
+                    'status' => 409
+                ]);
                 return false;
             }
             else
@@ -28,11 +32,21 @@ class View extends Component
                 ]);
 
                 session()->flash('message', 'Wishlist added successfully');
+                $this->dispatchBrowserEvent('message', [
+                    'text' => 'Wishlist added successfully',
+                    'type' => 'success',
+                    'status' => 200
+                ]);
             }
         }
         else
         {
             session()->flash('message', 'Please login to continue');
+            $this->dispatchBrowserEvent('message', [
+                'text' => 'Please login to continue',
+                'type' => 'info',
+                'status' => 401
+            ]);
 
             return false;
         }
