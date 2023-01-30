d9a6279064c51082f7de2578698c203643cf0f72
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-01-30 10:53:09 +0300

    Wishlist Remove Functionality is Added

--- app/Http/Livewire/Frontend/WishlistShow.php
+++ app/Http/Livewire/Frontend/WishlistShow.php
@@ -7,6 +7,17 @@ use Livewire\Component;
 
 class WishlistShow extends Component
 {
+    public function removeWishlistItem(int $wishlistId)
+    {
+        $wishlist = Wishlist::where('user_id', auth()->user()->id)->where('id', $wishlistId)->delete();
+
+        $this->dispatchBrowserEvent('message', [
+            'text' => 'Wishlist Item Remove Successfully',
+            'type' => 'success',
+            'status' => 200
+        ]);
+    }
+
     public function render()
     {
         $wishlist = Wishlist::where('user_id', auth()->user()->id)->get();
