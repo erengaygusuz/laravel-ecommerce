b24e23eeba2eb0f716a4f0bb18182aa892e9e76c
Eren Gaygusuz <gaygusuzeren@gmail.com>
2023-01-30 09:19:39 +0300

    Creating Wishlist model and wishlists Table

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -2,11 +2,41 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Wishlist;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class View extends Component
 {
     public $category, $product, $prodColorSelectedQuantity;
+
+    public function addToWishList($productId)
+    {
+        if (Auth::check())
+        {
+            if (Wishlist::where('user_id', auth()->user()->id)->where('product_id', $productId)->exists())
+            {
+                session()->flash('message', 'Already added to wishlist');
+
+                return false;
+            }
+            else
+            {
+                Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId
+                ]);
+
+                session()->flash('message', 'Wishlist added successfully');
+            }
+        }
+        else
+        {
+            session()->flash('message', 'Please login to continue');
+
+            return false;
+        }
+    }
 
     public function colorSelected($productColorId)
     {
